855902082076dbede966a5d7198c556a86186be5
davidmtereriu <davidmterner@gmail.com>
2021-12-03 11:05:41 -0500

    New commit style.

--- public/js/wowchemy-slides.js
+++ public/js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = {highlight_style: "dracula", theme: "black"};
 
-  // ns-hugo:/Users/davidmterner/Desktop/IU/cradle_grave_web/themes/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/assets/js/wowchemy-utils.js
+  // ns-hugo:/Users/davidmterner/Desktop/IU/Admin/Website/cradle_grave_web/themes/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/assets/js/wowchemy-utils.js
   function fixMermaid() {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
